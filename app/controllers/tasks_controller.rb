276c02888eddf9557324d2d7789e78d2bacef2d1
akhyra renomeron <khyrarenomeron@gmail.com>
2022-02-15 19:44:14 -0500

    no change

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,8 +13,8 @@ class TasksController < ApplicationController
 
   def create
     @tasks = Task.new(params[:task])
-    @tasks.save
     redirect_to tasks_path(@tasks)
+    @tasks.save
   end
 
   def edit
